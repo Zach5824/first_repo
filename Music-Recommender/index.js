02ef85d12b35045a28aac5fb186c3f7f07c4f30b
Zach5824 <kazokoth58@gmail.com>
2026-04-17 10:12:43 +0300

    test data

---
+++ Music-Recommender/index.js
@@ -0,0 +1,8 @@
+// 1. Your Mock Database
+const musicLibrary = [
+    { title: "Midnight City", artist: "M83", mood: "chill", img: "https://via.placeholder.com/150" },
+    { title: "Thunderstruck", artist: "AC/DC", mood: "energetic", img: "https://via.placeholder.com/150" },
+    { title: "Walking on Sunshine", artist: "Katrina", mood: "happy", img: "https://via.placeholder.com/150" },
+    { title: "Hello Darkness", artist: "Simon", mood: "sad", img: "https://via.placeholder.com/150" }
+];
+
